f7e92da283e7985d4f01bb8c4ec34ef6176e3181
Fathul Arifin <fyn.ifyn>
2024-10-20 05:01:01 +0200

    first commit

--- seeder/index.js
+++ seeder/index.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
+const fs = require("fs");
 require("dotenv").config();
 
 async function main() {
   /**--------------- Not allowed to be edited - start - --------------------- */
   const mongoUri = process.env.MONGODB_URI;
   const collection = process.env.MONGODB_COLLECTION;
-
   const args = process.argv.slice(2);
-
   const command = args[0];
   /**--------------- Not allowed to be edited - end - --------------------- */
 
@@ -18,14 +17,50 @@ async function main() {
   });
 
   // Define a schema for the collection
-  const schema = new mongoose.Schema({}, { strict: false });
+  const schema = new mongoose.Schema(
+    {
+      title: String,
+      year: Number,
+      genre: [String],
+      description: String,
+      director: String,
+      cast: [String],
+    },
+    { strict: false }
+  );
   const Model = mongoose.model(collection, schema);
 
   switch (command) {
     case "check-db-connection":
       await checkConnection();
       break;
-    // TODO: Buat logic fungsionalitas yg belum tersedia di bawah
+    case "bulk-insert":
+      const data = JSON.parse(fs.readFileSync("seed.json", "utf8"));
+      console.log(data);
+
+      for (const movie of data) {
+        const movieModel = new Model();
+        movieModel.title = movie.title;
+        movieModel.year = movie.year;
+        movieModel.genre = movie.genre;
+        movieModel.description = movie.description;
+        movieModel.director = movie.director;
+
+        await movieModel.save();
+      }
+      await bulkInsert();
+      break;
+    case "get-all":
+      const movieDatalGetAll = await Model.find();
+      console.log(movieDatalGetAll);
+
+      console.log("Get Data berhasil");
+      break;
+
+    case "reset-db":
+      await Model.deleteMany();
+      console.log("delete db berhasil");
+      break;
     default:
       throw Error("command not found");
   }
@@ -43,6 +78,10 @@ async function checkConnection() {
     console.error("MongoDB connection failed:", err);
   }
   console.log("check db connection ended...");
+}
+
+async function bulkInsert() {
+  console.log("bulk insert berhasil");
 }
 
 main();
